186b31bf5108a0542367d925115a02530956fd09
mmollerstrom <mmollerstrom@icloud.com>
2017-05-19 18:26:49 +0200

    fix up cors settings

--- config/application.rb
+++ config/application.rb
@@ -22,13 +22,14 @@ module CooperApi
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.api_only = true
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options, :head],
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options, :head],
                       expose: %w(access-token expiry token-type uid client),
                       max_age: 0
       end
